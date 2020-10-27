0bcf5da171ecb9f7185661de846c95041cc132bf
mego1907 <ahmedabdelmageed18@gmail.com>
2020-10-27 03:37:01 -0700

    5 UpLoad

---
+++ assets/js/main.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", ()=> {
+    // cat
+    const productLists = document.querySelectorAll('.shop .products-list ul li');
+    const catorgy = document.querySelectorAll('.shop .products-items .catorgy');
+    
+    for(let i = 0; i < productLists.length; i++) {
+        productLists[i].addEventListener('click', () => {
+            productLists.forEach((e)=> e.classList.remove('active'))
+            productLists[i].classList.add('active');
+            for(let x = 0; x < catorgy.length; x++) {
+                if(productLists[i].dataset.catorgy == catorgy[x].id) {
+                    catorgy.forEach((e)=> e.classList.remove('active'));
+                    catorgy[i].classList.add('active');
+                }
+            }
+            
+        })
+    }
+})
