ca7e86329069f4ca22ca0414b349dfdc5e7a89d5
mego1907 <ahmedabdelmageed18@gmail.com>
2020-10-31 05:00:50 +0200

    21 Upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,43 @@
-document.addEventListener("DOMContentLoaded", ()=> {
-    document.getElementById('create-acc').addEventListener('click',function(){
+// Navbar
+const navLink = document.querySelectorAll('.nav-item .nav-link'); // Navbar Link
 
-        document.querySelector('.sign .signin').style.display = 'none';
-        document.querySelector('.sign .signup').style.display = 'block';
+for(let i =0; i < navLink.length; i++) {
+    // Add Active Class When I Click On NavLink 
+    navLink[i].addEventListener('click', ()=> {
+        navLink.forEach((e)=> e.classList.remove('active')) // Remove Active
+        navLink[i].classList.add('active'); // Add Active
+    })
+}
 
-    });
+
+
+// cart
+const productLists = document.querySelectorAll('.shop .products-list ul li'); // List Item
+const catorgy = document.querySelectorAll('.shop .products-items .catorgy'); // Catorgy
+
+for(let i = 0; i < productLists.length; i++) {
+    productLists[i].addEventListener('click', () => {
+        productLists.forEach((e)=> e.classList.remove('active'))
+        productLists[i].classList.add('active');
+        for(let x = 0; x < catorgy.length; x++) {
+            if(productLists[i].dataset.catorgy == catorgy[x].id) {
+                catorgy.forEach((e)=> e.classList.remove('active'));
+                catorgy[i].classList.add('active');
+            }
+        }
+            
+    })    
+}
+
+
+/* Sign in Page */
+document.querySelector('#btnCreate').addEventListener('click',function(){
+    document.querySelector('.sign .signin').style.display = 'none'; // hide signin section
+    document.querySelector('.sign .signup').style.display = 'block'; // show signup section
 });
 
-document.addEventListener("DOMContentLoaded", ()=> {
-    // cart
-    const productLists = document.querySelectorAll('.shop .products-list ul li');
-    const catorgy = document.querySelectorAll('.shop .products-items .catorgy');
-    
-    for(let i = 0; i < productLists.length; i++) {
-        productLists[i].addEventListener('click', () => {
-            productLists.forEach((e)=> e.classList.remove('active'))
-            productLists[i].classList.add('active');
-            for(let x = 0; x < catorgy.length; x++) {
-                if(productLists[i].dataset.catorgy == catorgy[x].id) {
-                    catorgy.forEach((e)=> e.classList.remove('active'));
-                    catorgy[i].classList.add('active');
-                }
-            }
-            
-        })
-    }
+
+document.querySelector('#innerSignin').addEventListener('click',function(){
+    document.querySelector('.sign .signup').style.display = 'none'; // hide signin section
+    document.querySelector('.sign .signin').style.display = 'block'; // show signup section
 });
